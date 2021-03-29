5c0d5ab501f12647ddb9a19c2adba6cb35d21d77
eunjeeSung <ismkaleidoscope@gmail.com>
2021-03-28 17:17:21 -0700

    Group into a class

--- ch2/rock_paper_scissors.py
+++ ch2/rock_paper_scissors.py
@@ -31,6 +31,8 @@ elif human_choice == 'scissors':
         print('Draw!')
 '''
 
+# Split into functions
+
 import random
 
 OPTIONS = ['rock', 'paper', 'scissors']
@@ -71,3 +73,50 @@ print_options()
 human_choice = get_human_choice()
 computer_choice = get_computer_choice()
 print_result(human_choice, computer_choice)
+
+
+# Group into a class
+class RockPaperScissorsSimulator:
+    def __init__(self):
+        self.human_choice = None
+        self.computer_choice = None
+
+    def print_options(self):
+        print('\n'.join(f"({i+1}) {option.capitalize()}" \
+            for i, option in enumerate(OPTIONS)))
+
+    def get_human_choice(self):
+        self.human_choice = \
+            OPTIONS[int(input('Enter the number of your choice: ')) - 1]
+        print(f'You chose {self.human_choice}')
+
+    def get_computer_choice(self):
+        self.computer_choice = random.choice(OPTIONS)
+        print(f'The computer chose {self.computer_choice}')
+
+    def get_human_wins(self):
+        if self.human_choice == self.computer_choice:
+            return 'Draw'
+        elif WINNING_OPTIONS[self.human_choice] == self.computer_choice:
+            return 'Win'
+        elif WINNING_OPTIONS[self.computer_choice] == self.human_choice:
+            return 'Lose'
+
+    def print_result(self):
+        result = self.get_human_wins()
+        if result == 'Win':
+            print(f'Yes, {self.human_choice} beat {self.computer_choice}!')
+        elif result == 'Lose':
+            print(f'Sorry, {self.computer_choice} beat {self.human_choice}')        
+        elif result == 'Draw':
+            print(f'Draw!')
+
+    def simulate(self):
+        self.print_options()
+        self.get_human_choice()
+        self.get_computer_choice()
+        self.print_result()
+
+
+RPS = RockPaperScissorsSimulator()
+RPS.simulate()
